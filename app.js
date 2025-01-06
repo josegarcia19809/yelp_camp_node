2bbd3354a996bec5b6d99e00ddc658f3fe588211
josegarcia19809 <josegarcia19809@gmail.com>
2025-01-06 15:20:53 -0600

    YelpCamp: Campgrounds CRUD - Campground Model Basics

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to DB!');
+})
+
 const app = express();
 const path = require('path');
 
@@ -7,6 +21,17 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
     res.render("home");
+})
+
+app.get('/makecampground', async (req, res) => {
+    const campground = new Campground(
+        {
+            title: "My Backyard",
+            description: "Cheap Backyard",
+        }
+    );
+    await campground.save();
+    res.send(campground);
 })
 
 app.listen(3000, () => {
